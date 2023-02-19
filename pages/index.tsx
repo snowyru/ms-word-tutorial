0f637c9e13d0ab03d017aa677ca5f556550364c6
snowyru <97807830+snowyru@users.noreply.github.com>
2023-02-19 22:34:00 +0200

    added screen size protection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,49 @@
+"use client";
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import {motion, useInView} from 'framer-motion';
 import WordHome from '../public/WordHome.png';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import useWindowsDimensions from '../comps/useWindowDimensions';
+
+interface ScreenSize{
+  width: number;
+  height: number;
+}
 
 export default function Home() {
 
-  const ref=useRef(null)
-  const isInView = useInView(ref)
-
-  useEffect(() => {
-    console.log('Element in view: ', isInView)
-  }, [isInView])
-
+  const {height, width} =useWindowsDimensions();
+  
+  const isScreenSmall = width<1200 && height<700 ;
 
   return (
     <>
-      {/* <motion.div className="bg-red-300 fixed justify-center items-center text-center inset-1/4 h-fit z-40" >
-          <h1 className='text-4xl font-bold underline '>Screen size not just right, yet...</h1>
-          <p className='text-2xl' >Please adjust your screen size zoom</p>
-          <p className='text-2xl' >To do this please :</p>
-          <p className='text-2xl' >Please hold ctrl on the bottom left of the keyboard while scrolling with the wheel on the mouse</p>
-          <p className='text-2xl' >Still struggling? Ask for help!</p>
-      </motion.div> */}
+    <motion.div className="grid grid-rows-3 grid-flow-col w-screen h-screen p-28 bg-blue-100" >
+      {isScreenSmall && 
+      <motion.div className="bg-red-300 fixed justify-center items-center text-center inset-1/4 h-fit z-40" >
+          <h1 className='text-4xl font-bold underline underline-offset-4 pt-4'>Hey, your screen is too small, please maximise your window</h1>
+          {height}' x '{width}
+          <p className='text-2xl text-left px-4' >If this message is still here please adjust your screen size zoom</p>
+          <p className='text-2xl text-left px-4' >To do this :</p>
+          <p className='text-2xl text-left px-4' >Push and hold ctrl on the bottom left of the keyboard while scrolling with the wheel on the mouse</p>
+          <p className='text-2xl text-left px-4 pb-4' >Still struggling? Ask for help!</p>
+      </motion.div>
+      }
     {/* Instruction side card */}
-      <motion.div ref={ref} className='fixed border-blue-500 border-4 w-[407px] h-[720px] left-0 ml-24 mt-36 p-4'>
-        <h1 className='text-4xl font-old underline' >Instructions</h1>
+      <motion.div className=' relative border-blue-500 border-4 p-4 '>
+        <h1 className='text-4xl font-bold underline p-2' >Instructions :</h1>
+          <p className='text-2xl p-2'>1. Create a header</p>
       </motion.div>
     {/* Word env */}
-      <motion.div className="fixed border-red-500 border-4 w-[1280px] h-[720px] right-0 mr-24 mt-36" 
+      <motion.div className=" row-span-3 col-span-8 relative border-red-500 border-4 " 
         style={{}}
       >
         <Image src={WordHome} fill alt="..." />
       </motion.div>
+    </motion.div>
     </>
 
   )
