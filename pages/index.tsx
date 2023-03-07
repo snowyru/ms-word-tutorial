0c30c10abfea86162cf28e16c35c2afbf7a5d797
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-03-07 23:28:34 +0200

    reinstalled node with fontchg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 import styles from '@/styles/Home.module.css';
 import {motion, useInView} from 'framer-motion';
 import WordHome from '../public/WordHome.png';
